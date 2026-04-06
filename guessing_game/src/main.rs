e2a290103358a4a84ba24125c295ce9b59ed77de
Kabelo Dike <dikeair12@gmail.com>
2026-04-06 14:43:10 -0400

    first commit

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,50 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the Number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess:");
+
+    loop{
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a valid number!!");
+                continue;
+            }
+        };
+
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Too small");
+                println!("Please input your guess:");
+            },
+            Ordering::Greater => {
+                println!("Too big");
+                println!("Please input your guess:");
+            },
+            Ordering::Equal => {
+                 println!("You win");
+                 break;
+            }
+        }
+    }
+
+}
+
